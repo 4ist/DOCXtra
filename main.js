7ba0290719b713aabe48b4d295ecc327b3b0074e
4ist <frichardson062@gmail.com>
2020-10-22 20:37:45 -0500

    implemented parser engine into electron

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const url = require("url");
 const path = require("path");
+const docRunner = require("./doc-runner.ts");
+//import {DocRunner} from "./doc-runner.ts";
 
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -28,6 +30,15 @@ function createWindow () {
   })
 }
 
+function openModal() {
+  const { BrowserWindow } = require('electron');
+  let modal = new BrowserWindow({ parent: mainWindow, modal: true, show: false })
+  modal.loadURL('https://www.sitepoint.com')
+  modal.once('ready-to-show', () => {
+    modal.show()
+  })
+}
+
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
@@ -36,4 +47,22 @@ app.on('window-all-closed', function () {
 
 app.on('activate', function () {
   if (mainWindow === null) createWindow()
+})
+
+ipcMain.on('openModal', (event, arg) => {
+
+  const inputFile = 'testFiles/file-sample_100kB.docx';
+  const outputFile = 'output.docx';
+  const data = {
+    first_name: 'John',
+    last_name: 'Doe',
+    phone: '0652455478',
+    description: 'New Website',
+    receiver_first_name: 'Jane'
+  }; //TODO cast a string as a JS generic object
+
+  //openModal()
+  //const runner = new docRunner.DocRunner
+  docRunner.replaceDocData(inputFile, outputFile, data)
+  //replaceDocData()
 })
